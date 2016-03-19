dde7f08f77d3a364095a06b0e18fc582e7fd9fc7
James Lynch <jim@fantasyup.com>
2016-03-19 10:31:02 -0400

    after yeoman generator run

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('ngNjOrg')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
