09fd92505e054a81b5f66b11744422f05a96ba04
James Lynch <mrdotjim@gmail.com>
2016-06-15 20:23:47 -0500

    travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,15 @@
 'use strict';
 
+if (process.env.TRAVIS) {
+  config.sauceUser = 'JimTheMan';
+  config.sauceKey = '7c90507b-1930-450d-826e-6aa236b40e03';
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': '1',
+    'build': '1'
+  };
+}
+
 var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 
 // An example configuration file.
