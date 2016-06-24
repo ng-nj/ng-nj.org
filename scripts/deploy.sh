ec2b3175aec2865fc84e512b2054071bf503420c
JimTheMan <mrdotjim@gmail.com>
2016-06-23 23:38:55 -0400

    try it

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,6 +39,6 @@ echo "Deployed successfully."
 # git push origin :gh-pages
 
 git push origin :gh-pages
-git commit -am 'commit from a bashy bizzash'
+git commit -am 'commit from here.'
 git subtree push --prefix dist/ origin gh-pages
   
