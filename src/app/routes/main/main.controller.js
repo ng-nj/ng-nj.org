42e1afa261264f15c4374a6d7345a1cae92b1042
HomeboxJim <james.lynch@hbo.com>
2016-10-04 14:16:57 -0400

    it works!

--- src/app/routes/main/main.controller.js
+++ src/app/routes/main/main.controller.js
@@ -9,6 +9,17 @@
   function MainController($timeout, $log) {
     var vm = this;
 
+   	var config = {
+        	apiKey: "AIzaSyAWJp_C_eqNpQaPZsmKxIYzUOg5QgkNTTM",
+    		authDomain: "ng-nj.firebaseapp.com",
+    		databaseURL: "https://ng-nj.firebaseio.com",
+        	storageBucket: "project-537738856427405277.appspot.com",
+	    	messagingSenderId: "964546155082"
+	};
+	
+	firebase.initializeApp(config);
+
+    	console.log('firebase var is: ' + firebase.storage().ref());
 
   }
 })();
